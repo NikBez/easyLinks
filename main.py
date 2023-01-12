1d878d3fe42fd2785f4e213998897f737ec079ca
Kit <nikitabit@yandex.ru>
2023-01-12 11:43:25 +0700

    Make enhancement of code

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
-import os
+import os, sys
+from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-token = os.environ['BITLINK_ACCESS_TOKEN']
-
 
 def main():
-    link = input('Your link: ')
-    # Проверяем какую ссылку ввел пользователь, если короткую показываем сколько было кликов
-    if is_bitlink(link):
+    load_dotenv()
+    token = os.environ['BITLINK_ACCESS_TOKEN']
+    user_link = input('Your link: ')
+
+    if is_bitlink(user_link, token):
         try:
-            click_count = count_clicks(link)
+            click_count = count_clicks(user_link, token)
         except requests.exceptions.HTTPError:
             print("Can't find number of clicks for this link(")
-            exit()
+            sys.exit()
         print(f'This link has {click_count} clicks')
 
-    # Если длинную, то сокращаем
     else:
         try:
-            bitlink = shorten_link(link)
+            bitlink = shorten_link(user_link, token)
         except requests.exceptions.HTTPError:
             print("Link is invalid. ")
-            exit()
+            sys.exit()
 
         print('Bitlink: ', bitlink)
 
 
-# Функция возвращает  ссылку в сокращенном виде, использую внешний API bitlink
-def shorten_link(link):
+def shorten_link(link, token):
     url = "https://api-ssl.bitly.com/v4/bitlinks"
     headers = {"Authorization": f"Bearer {token}"}
-    data = {
+    link_params = {
         "long_url": link,
     }
 
-    bitlink = requests.post(url, json=data, headers=headers)
+    bitlink = requests.post(url, json=link_params, headers=headers)
     bitlink.raise_for_status()
     return bitlink.json()['link']
 
 
-# Функция возвращает количество переходов по входящей ссылке
-def count_clicks(bitlink):
-    bitlink = bitlink.split("//")[1] if '//' in bitlink else bitlink
+def count_clicks(bitlink, token):
+    bitlink_parts = urlparse(bitlink)
+    bitlink = bitlink_parts.netloc + bitlink_parts.path
 
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
     headers = {"Authorization": f"Bearer {token}"}
@@ -53,19 +51,21 @@ def count_clicks(bitlink):
         "units": -1,
     }
 
-    clicks = requests.get(url, params=params, headers=headers)
-    clicks.raise_for_status()
-    return clicks.json()["total_clicks"]
+    response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()
+    return response.json()["total_clicks"]
 
 
-# Функция проверяет переданную ссылку: сокращенная она или нет
-def is_bitlink(link):
-    link = link.split("//")[1] if '//' in link else link
+def is_bitlink(link, token):
+
+    link_parts = urlparse(link)
+    link = link_parts.netloc + link_parts.path
+
     headers = {"Authorization": f"Bearer {token}"}
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}"
 
-    clicks = requests.get(url, headers=headers)
-    return True if clicks.ok else False
+    response = requests.get(url, headers=headers)
+    return response.ok
 
 
 if __name__ == "__main__":
